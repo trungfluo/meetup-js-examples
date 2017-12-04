810a834fdcec36a335dbdbb3e494df20a9827554
Trung Nguyen <trung.nguyen@fluo.com>
2017-12-04 22:12:26 +0100

    feat(examples): add covariance and contravariance example

--- src/1.js
+++ src/1.js
@@ -1,2 +1,20 @@
 // @flow
 
+type Estimation = 'GOOD' | 'PASSABLE' | 'BAD' | 'NEUTRAL';
+type Vehicle = {
+  info: {
+    estimation: Estimation,
+  },
+};
+
+type ApiVehicle = {
+  info: {
+    estimation: ?Estimation,
+  },
+};
+
+function toApiVehicle(ref: Vehicle): ApiVehicle {
+  return {
+    info: ref.info,
+  };
+}
